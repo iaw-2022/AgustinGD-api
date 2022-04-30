0993c60e2dfd592853a15482baba046c94042890
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-04-30 14:09:06 -0300

    setup swaggerUi

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const server = express();
+const swaggerOptions = {
+    swaggerDefinition:{
+        info:{
+            title: "API granja",
+            description: "API para productos de la granja",
+            contact: {
+                name: "Agustin Emanuel Gonzalez Diaz"
+            },
+            servers: ["lamejorurl"]
+        }
+    },
+    apis: ["index.js"]
+}
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 const PORT = 5000;
-
 server.listen(PORT, () => {
     console.log('\n Bienvenido a la api')
 })
