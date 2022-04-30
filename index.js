e252ef1b706100f76cce05fe433efe57ddaf9ca1
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-04-30 19:09:38 -0300

    puerto segun env var

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require('./config');
 const express = require('express');
 
 const server = express();
@@ -5,7 +6,7 @@ const server = express();
 const swaggerConfig = require('./swaggerConfig');
 server.use('/api-docs', swaggerConfig.serve, swaggerConfig.setup);
 
-const PORT = 5000;
+const PORT = config.API_PORT || 5000;
 server.listen(PORT, () => {
-    console.log('\n Bienvenido a la api')
-})
+    console.log('\n Bienvenido a la api');
+});
