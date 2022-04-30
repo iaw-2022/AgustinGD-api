279deb3e826f0d8a37a7b96ae434a1c5404c5069
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-04-30 19:05:33 -0300

    separando swagger del index

---
+++ swaggerConfig.js
@@ -0,0 +1,24 @@
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const swaggerOptions = {
+    swaggerDefinition:{
+        info:{
+            title: "API granja",
+            description: "API para productos de la granja",
+            contact: {
+                name: "Agustin Emanuel Gonzalez Diaz"
+            },
+            servers: ["lamejorurl"]
+        }
+    },
+    apis: ["index.js"]
+}
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+
+const serve = swaggerUi.serve;
+const setup = swaggerUi.setup(swaggerDocs);
+module.exports = {
+    serve,
+    setup,
+};
