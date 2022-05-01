b2ae9efaa693b931f1a7c7d64d23de2fcee5f1e8
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-04-30 21:26:45 -0300

    usar config en vez de env var

--- database/knexfile.js
+++ database/knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
     production:{
         client: "pg",
         connection: { 
-            connectionString: process.env.DATABASE_URL,
+            connectionString: config.DATABASE_URL,
             ssl: { rejectUnauthorized: false }
         },
         pool:{
