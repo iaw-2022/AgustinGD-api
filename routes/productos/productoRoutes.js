51d59aa7c6c53669f17e55c774c7984016adb01a
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-05-08 19:45:53 -0300

    Mejor documentacion para get all productos

--- routes/productos/productoRoutes.js
+++ routes/productos/productoRoutes.js
@@ -7,10 +7,14 @@ const router = express.Router();
  * @swagger
  * /api/productos:
  *  get:
- *      description: Recupera todos los productos
+ *      description: Recupera todos los productos. 
+ *      tags: 
+ *       - Productos
  *      responses:
  *          '200':
  *              description: Respuesta exitosa
+ *          '500':
+ *              description: Error del servidor
 */
 router.get('/', productoController.getAll);
 
