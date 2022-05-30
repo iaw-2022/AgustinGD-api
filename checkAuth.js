d0d66c7d53d43995766d8702f0471741d1e07579
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-05-30 06:36:12 -0300

    indentacion

--- checkAuth.js
+++ checkAuth.js
@@ -3,11 +3,11 @@ var jwks = require('jwks-rsa');
 const config = require('./config')
 
 var jwtCheck = jwt({
-      secret: jwks.expressJwtSecret({
-          cache: true,
-          rateLimit: true,
-          jwksRequestsPerMinute: 5,
-          jwksUri: config.AUTH0_JWKSURI,
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: config.AUTH0_JWKSURI,
     }),
     audience: config.AUTH0_AUDIENCE,
     issuer: config.AUTH0_ISSUER,
